d7803f27cbbef42f9cbf7b7587d70619a191f53f
Dmitry Lapynov <dmitry.lapynov@corp.mail.ru>
2017-05-05 19:25:15 +0300

    Native utf8 encoding;

--- oksdk.js
+++ oksdk.js
@@ -525,47 +525,11 @@ var OKSDK = (function () {
     }
 
     function encodeUtf8(string) {
-        var res = "";
-        for (var n = 0; n < string.length; n++) {
-            var c = string.charCodeAt(n);
-            if (c < 128) {
-                res += String.fromCharCode(c);
-            }
-            else if ((c > 127) && (c < 2048)) {
-                res += String.fromCharCode((c >> 6) | 192);
-                res += String.fromCharCode((c & 63) | 128);
-            }
-            else {
-                res += String.fromCharCode((c >> 12) | 224);
-                res += String.fromCharCode(((c >> 6) & 63) | 128);
-                res += String.fromCharCode((c & 63) | 128);
-            }
-        }
-        return res;
+        return unescape(encodeURIComponent(string));
     }
 
     function decodeUtf8(utftext) {
-        var string = "";
-        var i = 0;
-        var c = 0, c2 = 0, c3 = 0;
-        while (i < utftext.length) {
-            c = utftext.charCodeAt(i);
-            if (c < 128) {
-                string += String.fromCharCode(c);
-                i++;
-            }
-            else if ((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i + 1);
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                i += 2;
-            } else {
-                c2 = utftext.charCodeAt(i + 1);
-                c3 = utftext.charCodeAt(i + 2);
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                i += 3;
-            }
-        }
-        return string;
+        return decodeURIComponent(escape(utftext));
     }
 
     /** stub func */
